8e756381189e06fde43210e5bb6eb0d3cdeaec84
Hann0T <hansolivas1@gmail.com>
2022-10-10 21:50:02 -0500

    refactor: the js files are published on /vendor/webauthn.

--- src/WebauthnServiceProvider.php
+++ src/WebauthnServiceProvider.php
@@ -388,7 +388,7 @@ class WebauthnServiceProvider extends ServiceProvider
             ], 'webauthn-migrations');
 
             $this->publishes([
-                __DIR__.'/../resources/js' => public_path('vendor/webauthn'),
+                __DIR__.'/../resources/js' => resource_path('js/vendor/webauthn'),
             ], 'webauthn-assets');
 
             $this->publishes([
